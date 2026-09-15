a981728df2a1fc5282b16004a523a6bdb0987e30
hmode1k <hmode288277@gmail.com>
2026-09-15 23:19:11 +0300

    implemented commentsRouter

--- routes/commentsRouter.js
+++ routes/commentsRouter.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
+const commentsController = require("../controllers/commentsController");
 
 const commentsRouter = Router({ mergeParams: true });
 
-commentsRouter.get("/"); //get all comments for the postId
-commentsRouter.post("/"); //add a new comment to the postId
-commentsRouter.delete("/:id"); // delete comment
+commentsRouter.get("/", commentsController.getAllComments); //get all comments for the postId
+commentsRouter.post("/", commentsController.addComment); //add a new comment to the postId
+commentsRouter.delete("/:commentId", commentsController.deleteComment); // delete comment
 
 module.exports = { commentsRouter };
--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const postsController = require("../controllers/postsController");
+const { commentsRouter } = require("./commentsRouter");
 
 const postsRouter = Router();
 
@@ -7,5 +8,6 @@ postsRouter.get("/", postsController.getAllPosts); //get all posts
 postsRouter.post("/", postsController.addPost); // add a new post
 postsRouter.get("/:id", postsController.getPost); //get specific post
 postsRouter.get("/:id/delete", postsController.deletePost); // delete specific post
+postsRouter.use("/postId/", commentsRouter);
 
 module.exports = { postsRouter };
