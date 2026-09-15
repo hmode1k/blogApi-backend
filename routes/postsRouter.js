a4e3cf5bc13cd07ea6d7b3e786fed5bc643f893c
hmode1k <hmode288277@gmail.com>
2026-09-15 23:23:21 +0300

    fixed missing colons in postsRouter

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -8,6 +8,6 @@ postsRouter.get("/", postsController.getAllPosts); //get all posts
 postsRouter.post("/", postsController.addPost); // add a new post
 postsRouter.get("/:id", postsController.getPost); //get specific post
 postsRouter.get("/:id/delete", postsController.deletePost); // delete specific post
-postsRouter.use("/postId/", commentsRouter);
+postsRouter.use("/:postId/comments", commentsRouter);
 
 module.exports = { postsRouter };
