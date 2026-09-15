7f519a07fac58d478892a2fb1c101ca3d4775b8f
hmode1k <hmode288277@gmail.com>
2026-09-15 23:25:09 +0300

    fixed a typo in commentsRouter

--- routes/commentsRouter.js
+++ routes/commentsRouter.js
@@ -5,6 +5,6 @@ const commentsRouter = Router({ mergeParams: true });
 
 commentsRouter.get("/", commentsController.getAllComments); //get all comments for the postId
 commentsRouter.post("/", commentsController.addComment); //add a new comment to the postId
-commentsRouter.delete("/:commentId", commentsController.deleteComment); // delete comment
+commentsRouter.get("/:commentId/delete", commentsController.deleteComment); // delete comment
 
 module.exports = { commentsRouter };
