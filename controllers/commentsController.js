ca1e084dc3c61d3e5d40d792d9262560f7dce9a7
hmode1k <hmode288277@gmail.com>
2026-09-15 23:16:59 +0300

    implemented comments controller

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -1,3 +1,24 @@
+const { use } = require("passport");
 const db = require("../db/queries");
 
+async function getAllComments(req, res) {
+  const postId = req.params.postId;
+  const comments = await db.getAllComments(postId);
+  res.send(comments);
+}
 
+async function addComment(req, res) {
+  const { comment } = req.body;
+  const postId = req.params.postId;
+  const userId = 1; //place holder value until i implement auth
+  await db.addComment(comment, postId, userId);
+  res.redirect("/");
+}
+
+async function deleteComment(req, res) {
+  const id = req.params.commentId;
+  await db.deleteComment(id);
+  res.redirect("/");
+}
+
+module.exports = { getAllComments, addComment, deleteComment };
