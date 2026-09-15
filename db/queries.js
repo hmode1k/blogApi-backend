0b4672cfe724ded7c092460a8b9c8b7f438c61a7
hmode1k <hmode288277@gmail.com>
2026-09-15 23:12:36 +0300

    added comments crud queries

--- db/queries.js
+++ db/queries.js
@@ -21,9 +21,36 @@ async function deletePost(id) {
   await pool.query("DELETE FROM posts WHERE id = $1", [id]);
 }
 
+// comments queries section
+
+// this function get all the comments on a specific post based on the post id
+async function getAllComments(postId) {
+  const { rows } = await pool.query(
+    "SELECT * FROM commnts WHERE post_id = $1",
+    [postId],
+  );
+  return rows;
+}
+
+// add a comment to a post
+async function addComment(comment, postId, userId) {
+  await pool.query(
+    "INSERT INTO commnts (comment, post_id, user_id) VALUES ($1, $2, $3)",
+    [comment, postId, userId],
+  );
+}
+
+//delete comment based on comment id
+async function deleteComment(id) {
+  await pool.query("DELETE FROM commnts WHERE id = $1", [id]);
+}
+
 module.exports = {
   getAllPosts,
   getPost,
   addPost,
   deletePost,
+  getAllComments,
+  addComment,
+  deleteComment,
 };
